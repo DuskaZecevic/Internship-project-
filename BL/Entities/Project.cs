4363310d7dfcca9a5ec8b374e7a09749524294e6
Duska <Duska@DESKTOP-O3AR66J>
2022-05-21 19:13:49 +0200

    last change

---
+++ BL/Entities/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Entities
+{
+    public class Project
+    {
+        public string nameOfProject { get; set; }
+        public DateTime? startDate { get; set; }
+        public DateTime? completionDate { get; set; }
+        public enum statusOfProject
+        {
+            NotStarted,
+            Active,
+            Completed
+        };
+
+        public int priorityOfProject { get; set; }
+    }
+}
---
+++ BL/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Entities
+{
+    public class Task
+    {
+        public string name { get; set; }
+        public enum status
+        {
+            ToDo,
+            InProgress,
+            Done
+        };
+        public string description { get; set; }
+        public int priority { get; set; }
+    }
+}
