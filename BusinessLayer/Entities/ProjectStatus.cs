7468964ac92c5dc4e90823715c8bf2945b17d31d
Duska <Duska@DESKTOP-O3AR66J>
2022-05-23 16:38:30 +0200

    Last change

--- BusinessLayer/Entities/ProjectStatus.cs
+++ BusinessLayer/Entities/ProjectStatus.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using BusinessLayer.Entities;
-
-namespace BusinessLayer.Entities
+﻿namespace BusinessLayer.Entities
 {
     public enum ProjectStatus
     {
